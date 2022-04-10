a7895d324e20589642c9520b861082fdc097a050
Dhaesha M <dhaesha1@gmail.com>
2022-04-10 15:05:32 -0400

    broken code. retrying this again

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
+
 import Queen from "./components/chess-queen/Chess-q.js";
 
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Canvas id="canvas-container">
-        <Suspense fallback={null}>
-          <Queen />
-        </Suspense>
-      </Canvas>
-    </div>
+    <Canvas id="canvas-container">
+      <Suspense fallback={null}>
+        <Queen />
+      </Suspense>
+    </Canvas>
   );
 }
-
-export default App;
